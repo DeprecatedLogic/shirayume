cbc756c00e0843542ca40ce4e3fbeb48f652fbd7
ItzKarizma <emertinaj2002@hotmail.com>
2025-09-25 18:04:00 +0200

    fix & feat: rename the Table class variables and values + add Rank class

--- utils/shared.py
+++ utils/shared.py
@@ -1,10 +1,10 @@
 from enum import Enum
 
 class Table(Enum):
-    users = "users"
-    guilds = "guilds"
+    user = "user"
+    guild = "guild"
     user_guild_settings = "user_guild_settings"
-    moderation_logs = "moderation_logs"
+    moderation_log = "moderation_log"
 
 class Action(Enum):
     warn = "warn"
@@ -23,3 +23,20 @@ class RPS(Enum):
 class CoinFlip(Enum):
     heads = "heads"
     tails = "tails"
+
+class Rank(Enum):
+    rank_0 = "Wet Sock"
+    rank_1 = "Microwave Enjoyer"
+    rank_2 = "Left Toe Collector"
+    rank_3 = "Certified Simp"
+    rank_4 = "Hotdog Without Bun"
+    rank_5 = "69% Charged Phone"
+    rank_6 = "Accidental Incognito Tab"
+    rank_7 = "OnlyFans Intern"
+    rank_8 = "Half-Eaten Burrito"
+    rank_9 = "Big Spoon (no cereal)"
+    rank_10 = "Banana Peeler Supreme"
+    rank_11 = "Mom's Credit Card"
+    rank_12 = "Horny Jail Inmate"
+    rank_13 = "Professional Stepbro"
+    rank_14 = "CEO of Bad Decisions"
