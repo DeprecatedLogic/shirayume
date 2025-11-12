42a6293497b84df40b18cc373c580c1cb7d42608
ItzKarizma <emertinaj2002@hotmail.com>
2025-11-12 11:34:48 +0100

    refactor(moderation_service): rename function, return model instead of None

--- services/moderation_service.py
+++ services/moderation_service.py
@@ -1,13 +1,14 @@
 from utils import shared, helpers
 from database import database_manager
 
-def import_moderation_logs(**kwargs):
+def add_moderation_logs(**kwargs):
     DB_MANAGER = database_manager.DB_MANAGER
     if not DB_MANAGER:
         helpers.custom_print(
             level = shared.LogLevel.CRITICAL,
-            function_name = "import_moderation_logs",
+            function_name = "add_moderation_logs",
             description = f"DB_MANAGER ({DB_MANAGER}) has not been initialized"
         )
     model = DB_MANAGER.initialize_database_model(shared.Table.moderation_logs, **kwargs)
     DB_MANAGER.add_moderation_logs(model)
+    return model
